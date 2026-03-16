ecf20dc28c8e9d37c24e1527d7032ff09f743cc6
parag <paraggamara525@gmail.com>
2026-03-16 06:55:45 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ⭐ Important for relative paths on Vercel
+  base: "./"  // Important for relative paths in production
 });
